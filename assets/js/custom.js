b2d5e8929280c38cc90b285ce8e8db39d75573be
Alberto Pettarin <alberto@albertopettarin.it>
2016-04-08 22:15:47 +0200

    More sample tests

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,25 +12,38 @@ if (data === undefined) {
 
 // set title
 if (data["title"] !== undefined) {
-    $("#content").append("<h1>" + data["title"] + "</h1>");
-    $("#content").append("<hr/>");
+    var div;
+    div  = "<div class=\"row\">";
+    div += " <h1>" + data["title"] + "</h1>"
+    div += "</div>";
+    $("#content").append(div);
 }
 
 // if index
 if (data["type"] === "index") {
-    var div = "<div>";
-    div += "<p>Available plots:</p>";
-    div += "<ul>";
+    var div;
+    div  = "<div class=\"row\">";
+    div += " <table class=\"table table-striped\">";
+    div += "  <thead>";
+    div += "  <tr></tr>";
+    div += "   <th>#</th>";
+    div += "   <th>ID</th>";
+    div += "   <th>Description</th>";
+    div += "  </tr>";
+    div += "  </thead>";
+    div += "  <tbody>";
     for (var i=0; i < data["links"].length; ++i) {
         var el = data["links"][i];
-        div += "<li>";
-        div += "<a href=\"" + el["id"] + ".html\">" + el["description"] + "</a>";
-        div += "</li>";
+        div += "   <tr>";
+        div += "    <td>" + (i+1) + "</td>";
+        div += "    <td><a href=\"" + el["id"] + ".html\">" + el["id"] + "</a></td>";
+        div += "    <td>" + el["description"] + "</td>";
+        div += "   </tr>";
     } 
-    div += "</ul>";
+    div += "  </tbody>";
+    div += " </table>";
     div += "</div>";
     $("#content").append(div);
-    $("#content").append("<hr/>");
 }
 // end if index
 
@@ -38,6 +51,41 @@ if (data["type"] === "index") {
 if (data["type"] === "single") {
     var div;
 
+    div  = "<div class=\"row\">";
+    div += " <div class=\"col-md-4\">";
+    div += "  <table class=\"table table-striped\">";
+    div += "   <h2>Configuration</h2>";
+    div += "   <thead>";
+    div += "   <tr><th>Property</th><th>Value</th></tr>";
+    div += "   </thead>";
+    div += "   <tbody>";
+    div += "    <tr><td>ID</td><td>" + data["raw"]["id"] + "</td></tr>";
+    div += "    <tr><td>Description</td><td>" + data["raw"]["configuration"]["description"] + "</td></tr>";
+    div += "    <tr><td>Text length (frags)</td><td>" + data["raw"]["configuration"]["text_length"] + "</td></tr>";
+    div += "    <tr><td>Audio length (s)</td><td>" + d3.format(".03f")(data["raw"]["configuration"]["audio_length"]) + "</td></tr>";
+    div += "    <tr><td>Download</td><td><a class=\"btn btn-primary btn-sm\" href=\"results/" + data["raw"]["id"] + ".out.json\" role=\"button\">JSON</a></td></tr>";
+    div += "   </tbody>";
+    div += "  </table>";
+    div += " </div>";
+    div += " <div class=\"col-md-8\">";
+    div += "  <table class=\"table table-striped\">";
+    div += "   <h2>Execution</h2>";
+    div += "   <thead>";
+    div += "   <tr><th>Property</th><th>Value</th></tr>";
+    div += "   </thead>";
+    div += "   <tbody>";
+    div += "    <tr><td>Machine</td><td>" + data["machine"] + "</td></tr>";
+    div += "    <tr><td>Run time avg/std/min/max (s)</td><td>" + d3.format(".03f")(data["raw"]["total_mean"]) + " / " + d3.format(".03f")(data["raw"]["total_std"]) + " / " + d3.format(".03f")(data["raw"]["total_min"]) + " / " + d3.format(".03f")(data["raw"]["total_max"]) + "</td></tr>";
+    div += "    <tr><td>RTF avg/std/min/max</td><td>" + d3.format(".03f")(data["raw"]["rtf_mean"]) + " / " + d3.format(".03f")(data["raw"]["rtf_std"]) + " / " + d3.format(".03f")(data["raw"]["rtf_min"]) + " / " + d3.format(".03f")(data["raw"]["rtf_max"]) + "</td></tr>";
+    div += "   </tbody>";
+    div += "  </table>";
+    div += " </div>"; 
+    div += "</div>";
+    $("#content").append(div);
+    $("#content").append("<hr/>");
+
+    /*
+    // two columns graphs
     div  = "<div class=\"row\">";
     div += " <div class=\"col-md-6\">";
     div += "  <div id=\"chart1\"></div>";
@@ -48,32 +96,66 @@ if (data["type"] === "single") {
     div += "</div>";
     $("#content").append(div);
     $("#content").append("<hr/>");
+    */
 
-    div  = "<h2>Summary</h2>";
+    // one column graphs
+    div  = "<div class=\"row\">";
+    div += " <div id=\"chart1\"></div>";
+    div += "</div>";
     div += "<div class=\"row\">";
-    div += " <div class=\"col-md-6\">";
-    div += "  <ul>";
-    div += "   <li>ID: " + data["raw"]["id"] + "</li>";
-    div += "   <li>Description: " + data["raw"]["configuration"]["description"] + "</li>";
-    div += "   <li>Text length: " + data["raw"]["configuration"]["text_length"] + "</li>";
-    div += "   <li>Audio length: " + d3.format(".03f")(data["raw"]["configuration"]["audio_length"]) + "</li>";
-    div += "  </ul>";
-    div += " </div>";
-    div += " <div class=\"col-md-6\">";
-    div += "  <ul>";
-    div += "   <li>Machine: " + data["machine"] + "</li>";
-    div += "   <li>Total mean time: " + d3.format(".03f")(data["raw"]["total_mean"]) + "</li>";
-    div += "   <li>RTF mean: " + d3.format(".03f")(data["raw"]["rtf_mean"]) + "</li>";
-    div += "  </ul>";
-    div += " </div>";
+    div += " <div id=\"chart2\"></div>";
     div += "</div>";
     $("#content").append(div);
     $("#content").append("<hr/>");
 
-    div = "<h2>Configuration</h2>";
-    div += "<pre>";
+    div  = "<div class=\"row\">";
+    div += " <h2>Detail</h2>";
+    div += " <table class=\"table table-hover\">";
+    div += "  <thead>";
+    div += "  <tr></tr>";
+    div += "   <th>#</th>";
+    div += "   <th>Step</th>";
+    div += "   <th>Run time Avg (s)</th>";
+    div += "   <th>Run time Avg (%)</th>";
+    div += "   <th>Run time Std (s)</th>";
+    div += "   <th>Run time Min (s)</th>";
+    div += "   <th>Run time Max (s)</th>";
+    div += "  </tr>";
+    div += "  </thead>";
+    div += "  <tbody>";
+    for (var i=0; i < data["raw"]["steps_mean_lab"].length; ++i) {
+        var el = data["raw"]["steps_mean_lab"][i];
+        div += "   <tr>";
+        div += "    <td>" + (i+1) + "</td>";
+        div += "    <td>" + data["raw"]["labels"][i] + "</td>";
+        div += "    <td>" + d3.format(".03f")(data["raw"]["steps_mean"][i]) + "</td>";
+        div += "    <td>" + d3.format(".01f")(data["raw"]["steps_mean"][i] / data["raw"]["total_mean"] * 100) + "</td>";
+        div += "    <td>" + d3.format(".03f")(data["raw"]["steps_std"][i]) + "</td>";
+        div += "    <td>" + d3.format(".03f")(data["raw"]["steps_min"][i]) + "</td>";
+        div += "    <td>" + d3.format(".03f")(data["raw"]["steps_max"][i]) + "</td>";
+        div += "   </tr>";
+    }    
+    div += "   <tr>";
+    div += "    <td></td>";
+    div += "    <td><strong>total</strong></td>";
+    div += "    <td>" + d3.format(".03f")(data["raw"]["total_mean"]) + "</td>";
+    div += "    <td></td>";
+    div += "    <td>" + d3.format(".03f")(data["raw"]["total_std"]) + "</td>";
+    div += "    <td>" + d3.format(".03f")(data["raw"]["total_min"]) + "</td>";
+    div += "    <td>" + d3.format(".03f")(data["raw"]["total_max"]) + "</td>";
+    div += "   </tr>";
+    div += "  </tbody>";
+    div += " </table>";
+    div += "</div>";
+    $("#content").append(div);
+    $("#content").append("<hr/>");
+
+
+    div  = "<div class=\"row\">";
+    div += " <h2>Configuration</h2>";
+    div += " <pre>";
     div += JSON.stringify(data["raw"]["configuration"], null, "  ");
-    div += "</pre>";
+    div += " </pre>";
     div += "</div>";
     $("#content").append(div);
     $("#content").append("<hr/>");
@@ -100,14 +182,14 @@ if (data["type"] === "single") {
             "columns": [his_lab, his_val, his_cum],
             "types": {
                 "v": "bar",
-                "c": "area",
+                "c": "step",
             },
             "names": {
-                "v": "time",
-                "c": "cumulated",
+                "v": "step run time",
+                "c": "cumulated run time",
             },
             "groups": [["v"], ["c"]],
-            "labels": false,
+            "labels": true 
         },
         "axis": {
             "x": {
@@ -117,6 +199,10 @@ if (data["type"] === "single") {
             "y": {
                 "label": "s"
             }
+        },
+        "legend": {
+            "show": true,
+            "position": "right"
         }
     });
 
@@ -126,6 +212,10 @@ if (data["type"] === "single") {
             "columns": pie_data,
             "type": "pie",
         },
+        "legend": {
+            "show": true,
+            "position": "right"
+        }
     });
 }
 // end if single
